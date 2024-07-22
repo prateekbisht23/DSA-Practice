843cfcb2f7dd47a7d4a0f87e41b9e2eaf5e771f9
prateekbisht23 <prateekbisht04@gmail.com>
2024-07-22 12:30:24 +0530

    Ques2

---
+++ Ques2.java
@@ -0,0 +1,7 @@
+public class Ques2 {
+    public static void main(String[] args) {
+        for(int i = 0; i < args[0].length(); i++){
+            System.out.println(args[0].substring(0, i+1));
+        }
+    }
+}
