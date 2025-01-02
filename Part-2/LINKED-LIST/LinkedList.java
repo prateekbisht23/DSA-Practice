9c339283cb281db42107006d13629eecb6e5ea28
prateekbisht23 <prateekbisht04@gmail.com>
2025-01-02 12:54:12 +0530

    Linked List Delete Operation

--- Part-2/LINKED-LIST/LinkedList.java
+++ Part-2/LINKED-LIST/LinkedList.java
@@ -1,78 +1,68 @@
 
 import java.util.Scanner;
 
-class Node<T>{
+class Node<T> {
     T data;
     Node<T> next;
 
-    Node(T data){
+    Node(T data) {
         this.data = data;
     }
 
 }
 
-/* class Student{
-    int rollNo;
-    String name;
-    int marks[] = new int[3];
-} */
+/*
+ * class Student{
+ * int rollNo;
+ * String name;
+ * int marks[] = new int[3];
+ * }
+ */
 
+class LinkedListOperation<T> {
 
-class LinkedListOperation<T>{
-    
     Node<T> head;
     Node<T> tail;
 
     int length = 0;
 
-    LinkedListOperation(){
+    LinkedListOperation() {
 
     }
 
-    Node<T> createNode(T data){
+    Node<T> createNode(T data) {
         Node<T> node = new Node<>(data);
         return node;
     }
 
-    void addAtInd(int ind, T data){
-        if(ind == 0){
+    void addAtInd(int ind, T data) {
+        if (ind < 0 || ind > length) {
+            System.out.println("Invalid Index!! Can't Add!!");
+        } else if (ind == 0) {
             addAtBeg(data);
-        }
-        else if(ind == length){
+        } else if (ind == length) {
             addAtEnd(data);
-        }
-        else if (ind<length) {
-            addAtMid(data);
-        }
-        else if(ind > length){
-            System.out.println("Cannot add for Index greater than length");
+        } else {
+            Node<T> newNode = createNode(data);
+
+            Node<T> temp = head;
+
+            for (int i = 1; i < length; i++) {
+                temp = temp.next;
+            }
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+            length += 1;
         }
     }
 
-
-    void addAtMid(T data){
-        
-        Node<T> newNode = createNode(data);
-
-        Node<T> temp = head;
-        
-        for(int i=1; i<length/2; i++){
-            temp = temp.next;
-        }
-
-        newNode.next = temp.next;
-        temp.next = newNode;
-
-        length += 1;
-
-    }
-
-
-    void addAtBeg(T data){
+    void addAtBeg(T data) {
 
         Node<T> newNode = createNode(data);
 
-        if(head == null){
+        if (head == null) {
             head = newNode;
             tail = newNode;
             length += 1;
@@ -86,12 +76,11 @@ class LinkedListOperation<T>{
 
     }
 
-
-    void addAtEnd(T data){
+    void addAtEnd(T data) {
 
         Node<T> newNode = createNode(data);
-        
-        if(head == null){
+
+        if (head == null) {
             head = newNode;
             tail = newNode;
 
@@ -105,24 +94,88 @@ class LinkedListOperation<T>{
 
         length += 1;
 
-        /* Node<T> temp = head;
-        while(temp.next != null){
-            temp = temp.next;
-        }
-        temp.next = newNode; */
+        /*
+         * Node<T> temp = head;
+         * while(temp.next != null){
+         * temp = temp.next;
+         * }
+         * temp.next = newNode;
+         */
 
     }
 
-    void print(){
+    void deleteAtBeg() {
+        if (head == null) {
+            System.out.println("Linked List is Empty ... ");
+        } else if (head.next == null) {
+            head = null;
+        }
+
         Node<T> temp = head;
-        while(temp != null){
+        head = head.next;
+        temp = null;
+
+        length -= 1;
+
+    }
+
+    void deleteAtEnd() {
+        if (head == null) {
+            System.out.println("Linked List is Empty ... ");
+        } else if (head.next == null) {
+            head = null;
+        }
+
+        Node<T> temp = head;
+
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+        tail = temp;
+
+        length -= 1;
+
+    }
+
+    void deleteAtInd(int idx) {
+        if (idx < 0 || idx > length) {
+            System.out.println("Invalid Index!! Can't Delete!!");
+        } else if (idx == 0) {
+            deleteAtBeg();
+        } else if (idx == length) {
+            deleteAtEnd();
+        }
+
+        Node<T> temp = head;
+
+        for (int i = 0; i < idx - 1; i++) {
+            temp = temp.next;
+        }
+
+        Node<T> temp2 = temp.next;
+
+        temp.next = temp2.next;
+
+        temp2.next = null;
+        temp2 = null;
+
+        System.out.println("Node at " + idx + " Deleted ... ");
+
+        length -= 1;
+
+    }
+
+    void print() {
+        Node<T> temp = head;
+        while (temp != null) {
             System.out.println(temp.data);
             temp = temp.next;
         }
     }
 
 }
-
 
 public class LinkedList {
     public static void main(String[] args) {
@@ -132,16 +185,19 @@ public class LinkedList {
 
         Scanner sc = new Scanner(System.in);
 
-        while (true) { 
+        while (true) {
             System.out.println("1. Add at End");
             System.out.println("2. Add at Begening");
             System.out.println("3. Add at Index");
             System.out.println("4. Print");
+            System.out.println("5. Delete at End");
+            System.out.println("6. Delete at Begening");
+            System.out.println("7. Delete at Index");
             System.out.println("0. Exit");
             System.out.println("Enter the Choice - ");
             int choice = sc.nextInt();
 
-            switch(choice){
+            switch (choice) {
                 case 1 -> {
                     System.out.println("Enter a Value - ");
                     int data = sc.nextInt();
@@ -165,6 +221,22 @@ public class LinkedList {
                 case 4 -> {
                     System.out.println("The Linked List Is ... ");
                     obj.print();
+                }
+
+                case 5 -> {
+                    System.out.println("Node at end Deleted ... ");
+                    obj.deleteAtEnd();
+                }
+
+                case 6 -> {
+                    System.out.println("Node at Begening Deleted ... ");
+                    obj.deleteAtBeg();
+                }
+
+                case 7 -> {
+                    System.out.println("Enter an Index - ");
+                    int idx = sc.nextInt();
+                    obj.deleteAtInd(idx);
                 }
 
                 case 0 -> {
